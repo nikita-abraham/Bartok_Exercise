366ecea86da61e12dd5f5ddcc0146b3440edd53b
Nikita Abraham <nikitaa1995@gmail.com>
2015-11-19 00:14:45 -0500

    Player.cs + Checking Player.AddCard()

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -10,6 +10,9 @@ public class Bartok : MonoBehaviour {
 	public TextAsset layoutXML;
 	public Vector3 layoutCenter = Vector3.zero;
 
+	//the number of degrees to fan each card in a hand
+	public float handFanDegrees = 10f;
+
 	public bool ________________;
 
 	public Deck deck;
@@ -18,6 +21,9 @@ public class Bartok : MonoBehaviour {
 
 	public BartokLayout layout;
 	public Transform layoutAnchor;
+
+	public List<Player> players;
+	public CardBartok targetCard;
 
 	void Awake() {
 		S = this;
@@ -35,6 +41,7 @@ public class Bartok : MonoBehaviour {
 		layout.ReadLayout (layoutXML.text); //pass layoutXML to it
 
 		drawPile = UpgradeCardsList (deck.cards);
+		LayoutGame ();
 	}
 
 	//UpgradeCardsList casts the Cards in 1CD to be CardBartoks
@@ -45,5 +52,69 @@ public class Bartok : MonoBehaviour {
 			lCB.Add (tCD as CardBartok);
 		}
 		return(lCB);
+	}
+
+	//position all the cards in the drawpile properly
+	public void ArrangeDrawPile() {
+		CardBartok tCB;
+
+		for (int i=0; i<drawPile.Count; i++) {
+			tCB = drawPile [i];
+			tCB.transform.parent = layoutAnchor;
+			tCB.transform.localPosition = layout.drawPile.pos;
+			//rotation should start at 0
+			tCB.faceUp = false;
+			tCB.SetSortingLayerName (layout.drawPile.layerName);
+			tCB.SetSortOrder (-i * 4); //order them front to back
+			tCB.state = CBState.drawpile;
+		}
+	}
+
+	//perform the initial game layout
+	void LayoutGame() {
+		//create an empty GameObjectto serve as an anchor for the tableau
+		if(layoutAnchor == null) {
+			GameObject tGO = new GameObject("_LayoutAnchor");
+			//create an empty GameObject named _LayoutAnchor in the Hierarchy
+			layoutAnchor = tGO.transform; //grab its Transform
+			layoutAnchor.transform.position = layoutCenter;
+		}
+
+		//position the drawpile cards
+		ArrangeDrawPile ();
+
+		//set up the players
+		Player pl;
+		players = new List<Player> ();
+		foreach(SlotDef tSD in layout.slotDefs) {
+			pl = new Player();
+			pl.handSlotDef = tSD;
+			players.Add (pl);
+			pl.playerNum = players.Count;
+		}
+		players [0].type = PlayerType.human; // make the Oth player human
+	}
+
+	//the draw function will pull a single card from the drawPile and return it 
+	public CardBartok Draw() {
+		CardBartok cd = drawPile [0];
+		drawPile.RemoveAt (0);
+		return(cd);
+	}
+
+	//This Update method is used to test adding cards to players' hands 
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			players [0].AddCard (Draw ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			players [1].AddCard (Draw ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			players [2].AddCard (Draw ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			players [3].AddCard (Draw ());
+		}
 	}
 }
---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq; //enables LINQ queries
+
+	//the player can either be human or an AI
+
+public enum PlayerType {
+	human, 
+	ai
+}
+
+	//the individual player of the game
+	//note: player does NOT extend Monobehavior or any other class
+
+[System.Serializable] //make the player class visible in the Inspector pane
+	
+public class Player {
+
+	public PlayerType type = PlayerType.ai;
+	public int playerNum;
+
+	public List<CardBartok> hand; //the cards in this player's hand
+
+	public SlotDef handSlotDef;
+
+	//add a card to the hand
+	public CardBartok AddCard(CardBartok eCB) {
+		if (hand == null) hand = new List<CardBartok>();
+
+		// add the card to the hand
+		hand.Add (eCB);
+
+		return(eCB);
+	}
+
+	//remove a card from the hand
+	public CardBartok RemoveCard(CardBartok cb) {
+		hand.Remove (cb);
+
+		return(cb);
+	}
+}
